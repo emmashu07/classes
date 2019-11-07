f16f0a1d25ab9f08d6fe7633229110ae00890298
emmashu07 <370801@bsd48.org>
2019-11-06 21:30:36 -0800

    Edited.

--- games.cpp
+++ games.cpp
@@ -3,4 +3,16 @@
 
 using namespace std;
 
-Games::Games(char* 
+Games::Games(char* title, char* publisher, int year, int rating, int type) : Media(title, year, type){
+    this->publisher = publisher;
+    this->rating = rating;
+}
+
+char* Games::getPublisher() {
+    return publisher;
+}
+
+int Games::getRating() {
+    return rating;
+}
+    
