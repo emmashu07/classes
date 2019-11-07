6773966fca8dbf085669e67d467fa13abea43e46
emmashu07 <370801@bsd48.org>
2019-11-06 21:31:03 -0800

    Started.

---
+++ music.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "media.h"
+
+using namespace std;
+
+class Music : public Media {
+    public:
+        Music(char* title, char* artist, char* publisher, int year, int duration, int type);
+        char* getArtist();
+        char* getPublisher();
+        int getDuration();
+    private:
+        char* artist;
+        char* publisher;
+        int duration;
+};
