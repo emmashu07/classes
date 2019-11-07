f8a669b6bd0915ce18bf2c8a5ee252dd0c2308db
emmashu07 <370801@bsd48.org>
2019-11-06 21:29:51 -0800

    Edited.

--- media.h
+++ media.h
@@ -4,11 +4,12 @@ using namespace std;
 
 class Media {
 	public:
-		Media(char*, char*);
+		Media(char* title, int year, int type);
 		char* getTitle();
 		int getYear();
 		int getType();
 	private:
 		char* title;
 		int year;
+        int type;
 };
